8a26fd0b948dff435e4f8d9d1485d8c43193b7c4
vdebote <vdebote@veepee.com>
2022-05-09 01:00:31 +0200

    init

---
+++ GrpcLearning.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+
+Console.WriteLine("Hello, World!");
+
+Thread.Sleep(5_000);
+
+var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new Greeter.GreeterClient(channel);
+
+var names = new[] { "Sophie", "Patrick", "Romain", "Bernard", "Louise", "Marie" };
+var random = new Random();
+
+while (true)
+{
+
+    var response = await client.SayHelloAsync(new HelloRequest
+    {
+        Name = names[random.Next(0, names.Length)]
+    });
+
+    Console.WriteLine(response.Message);
+
+    Thread.Sleep(2_000);
+}
